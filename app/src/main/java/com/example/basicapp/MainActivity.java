253a7211eeaaa77f9ed17f7b8960a5ac58c6fd0c
Matt <matt@somewearlabs.com>
2026-03-12 17:48:51 -0700

    sync: auto-commit 2026-03-13T00:48:51.590Z

--- app/src/main/java/com/example/basicapp/MainActivity.java
+++ app/src/main/java/com/example/basicapp/MainActivity.java
@@ -18,6 +18,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -81,6 +82,10 @@ public class MainActivity extends AppCompatActivity implements
         Configuration.getInstance().setUserAgentValue(getPackageName());
 
         setContentView(R.layout.activity_main);
+
+        // Set up the custom toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         // Set up the navigation drawer
         drawerLayout = findViewById(R.id.drawerLayout);
